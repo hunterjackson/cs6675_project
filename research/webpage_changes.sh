3c68b7a0f18c727b1bd6fb6851cba3c8b6ec5b38
Hunter Jackson <hunter.jackson92@gmail.com>
2022-04-03 15:23:49 -0700

    add script to detect webpage changes and it's output

---
+++ research/webpage_changes.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SITE=$1
+
+fingerprint=`curl -s $SITE | sha256sum -t`
+ts=`date --iso-8601=seconds`
+
+echo "$ts| $fingerprint" 
+
+new_fingerprint=`curl -s $SITE | sha256sum -t`
+
+while [[ $fingerprint=$new_fingerprint ]] 
+do
+	new_fingerprint=`curl -s $SITE | sha256sum -t`
+	sleep 5s
+done
+
+ts=`date --iso-8601=seconds`
+echo "$ts| $new_fingerprint"
